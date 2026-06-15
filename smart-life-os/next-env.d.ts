56b19f70ea0d96493aa50626d23fb15a2db9ad87
Riptwosec-collab <aidsaras@gmail.com>
2026-06-15 15:57:31 +0700

    Add Smart Life OS Next env types

---
+++ smart-life-os/next-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// This file is automatically maintained by Next.js. It is included here so typecheck can run before first dev boot.
